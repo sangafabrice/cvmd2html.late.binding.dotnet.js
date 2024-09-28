c467bb114e532aef9303e597f629e50336f149ef
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-09-28 22:31:00 +0100

    refactor: Use the https service to download the JS library

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * @file Launches the shortcut target PowerShell script with the selected markdown as an argument.
  * It aims to eliminate the flashing console window when the user clicks on the shortcut menu.
- * @version 0.0.1.8
+ * @version 0.0.1.9
  */
 
 /** The application execution. */
 if (Param.Markdown) {
-  CreateConverter(Package.HtmlLibraryPath, Package.JsLibraryPath).ConvertFrom(Param.Markdown);
+  CreateConverter(Package.HtmlLibraryPath).ConvertFrom(Param.Markdown);
   quit(0);
 }
